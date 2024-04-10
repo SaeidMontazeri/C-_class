230d1882026779a8a1b11bf08bd204e9d3cdeb40
Saeid <saeidmontazeri@outlook.com>
2024-04-10 23:09:53 +0430

    sessin third added

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.txt4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(598, 419);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 36);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(449, 56);
+            this.txt1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(148, 30);
+            this.txt1.TabIndex = 1;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(826, 56);
+            this.txt2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(148, 30);
+            this.txt2.TabIndex = 2;
+            // 
+            // txt3
+            // 
+            this.txt3.Location = new System.Drawing.Point(449, 152);
+            this.txt3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(148, 30);
+            this.txt3.TabIndex = 3;
+            // 
+            // txt4
+            // 
+            this.txt4.Location = new System.Drawing.Point(826, 152);
+            this.txt4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txt4.Name = "txt4";
+            this.txt4.Size = new System.Drawing.Size(148, 30);
+            this.txt4.TabIndex = 4;
+           // this.txt4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 703);
+            this.Controls.Add(this.txt4);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.TextBox txt4;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(txt1.Text);
+            if (a % 7 == 0)
+            {
+                int r = Convert.ToInt32(txt2.Text);
+                float mohit, masahat;
+                mohit = 2 * 3 * r;
+                masahat = r * r * 3;
+                //MessageBox.Show("mohit: "+mohit.ToString()+"\n"+"masahat: "+masahat.ToString()) ;
+                //string.Format("mohit:{0}\n masahat:{1}", mohit, masahat);
+                MessageBox.Show($"mohit:{mohit}\nmasahat:{masahat}");
+            }
+            else
+            {
+                int x = Convert.ToInt32(txt3.Text);
+                int y= Convert.ToInt32(txt4.Text);
+                int sum = 0;
+                for(int i = x; i <= y; i++)
+                {
+                    if (i % 2 == 0)
+                        sum += i;
+                }
+                MessageBox.Show("sum is: "+sum.ToString());
+            }
+        }
+    }
+}
